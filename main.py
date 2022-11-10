1713564a7fbda27234cba40e1fd8c1497241bed6
ZiyodullaevJavohir <zhziedullaev@gmail.com>
2022-11-11 01:16:25 +0300

    Сбор информации о пользователе в классовой структуре

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-user_info: list = input("Введите ваши данные: ").split()
+class User:
+    def __init__(self, **kwargs):
+        self.user_info: dict = kwargs
+
+
+user = User(name="Жавохир", group="ПИЖ-б-о-21-1", course=2)
+
+for key, val in user.user_info.items():
+    print(f"{key}: {val}")
