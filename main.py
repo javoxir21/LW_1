3a6d31502f8e262a892d9d617c1b7626cc23d0ad
ZiyodullaevJavohir <zhziedullaev@gmail.com>
2022-11-11 02:02:15 +0300

    Расширение функционала менеджера пользователей

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-class UserManager:
-    users: list = list()
-
 class User:
-    def __init__(self, score: int, name: str, group: str):
-        self.id: int = len(UserManager.users)
-        UserManager().users.append(self)
+    def __init__(self, score: int, name: str, group: str, manager):
+        self.manager = manager
+        self.id: int = len(manager.user_list)
+        self.manager.user_list.append(self)
 
         self.name: str = name
         self.group: str = group
         self.score: int = score
 
+    def __str__(self) -> str:
+        return self.name
 
-user: User = User(score=3, name="Жавохир", group="ПИЖ-б-о-21-1")
+    def __del__(self):
+        self.manager.remove_user(user=self)
 
-print(f"ID: {user.id}\n"
-      f"Имя: {user.name}\n"
-      f"Группа: {user.group}\n"
-      f"Баллы: {user.score}\n")
+class UserManager:
+    def __init__(self):
+        self.user_list: list() = list()
+
+    def remove_user(self, user):
+        self.user_list.remove(user)
+        try:
+            del user
+        except Exception:
+            pass
+
+manager: UserManager = UserManager()
+
+javohir: User = User(score=3, name="Жавохир", group="ПИЖ-б-о-21-1", manager=manager)
+egor: User = User(score=5, name="Егор", group="ПИЖ-б-о-21-1", manager=manager)
+
+for user in manager.user_list:
+    print(user)
+
